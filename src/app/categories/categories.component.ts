1ea41a218538879b482f1ff06172c93d7e49a5b8
MayankMahajan007 <mayank@omnificence.co.in>
2025-06-03 17:28:04 +0530

    Changed Theme

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { SharedService } from  '../services/shared.service';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { SharedService } from '../services/shared.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,11 +9,23 @@ import { Router } from '@angular/router';
 })
 export class CategoriesComponent implements OnInit {
   selectedCategory: string = '';
+  dropdownOpen = false;
+
+  toggleDropdown(event: MouseEvent) {
+    event.stopPropagation(); // Prevent other click handlers
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  // Optional: close dropdown when clicking outside
+  @HostListener('document:click')
+  closeDropdown() {
+    this.dropdownOpen = false;
+  }
 
   constructor(private sharedService: SharedService, private route: Router) { }
 
   ngOnInit(): void {
-    if(this.sharedService.getSelectedCategory().length === 0) {
+    if (this.sharedService.getSelectedCategory().length === 0) {
       this.selectedCategory = 'featured';
       this.sharedService.setSelectedCategory(this.selectedCategory);
       this.sharedService.categorySubject.next(this.selectedCategory);
@@ -26,8 +38,8 @@ export class CategoriesComponent implements OnInit {
     this.selectedCategory = category;
     this.sharedService.setSelectedCategory(this.selectedCategory);
     this.route.navigate(['']);
-    setTimeout(()=>{
+    setTimeout(() => {
       this.sharedService.categorySubject.next(category);
-    }, 50); 
+    }, 50);
   }
 }
